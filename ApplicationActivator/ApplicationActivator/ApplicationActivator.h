a080ff2fdeda48abb11eeda37d9408d434085437
satohnorio <satohnorio@gmail.com>
2015-08-25 16:27:02 +0900

    VisualStudio2015 .NetFramework 4.5.1対応 名前空間を変更

---
+++ ApplicationActivator/ApplicationActivator/ApplicationActivator.h
@@ -0,0 +1,77 @@
+// ApplicationActivator.h
+
+#pragma once
+
+using namespace System;
+
+namespace ApplicationActivator {
+
+	public ref class ApplicationActivator
+	{
+		typedef cli::array<Diagnostics::Process^, 1> ProcessArray;
+
+	public:
+
+		ApplicationActivator() : FMyProcess(Diagnostics::Process::GetCurrentProcess())
+		{
+			Diagnostics::Process ^p = this->MyProcess;
+			String ^s = p->ProcessName;
+			this->Mutex = gcnew Threading::Mutex(false, s);
+		}
+
+		// ref classのデストラクタにはvirtual宣言は不要(C++/CLIの仕様)
+		~ApplicationActivator()
+		{
+			this->!ApplicationActivator();
+		}
+
+		!ApplicationActivator()
+		{
+			Threading::Mutex ^m = this->Mutex;
+			if (m != nullptr)
+			{
+				GC::KeepAlive(m);
+				m->Close();
+				this->Mutex = nullptr;
+			}
+		}
+
+		// 同じ実行ファイルが既に起動していればウィンドウをアクティブにする
+		// アクティブにできなければ（起動していなければ）falseを返す
+		bool Activate()
+		{
+			bool b = false;
+			if (!this->Mutex->WaitOne(0, false))
+			{
+				Diagnostics::Process ^myProcess = this->MyProcess;
+
+				ProcessArray ^hProcess = Diagnostics::Process::GetProcessesByName(myProcess->ProcessName);
+				for each (Diagnostics::Process ^p in hProcess)
+				{
+					b = (p->Id != myProcess->Id);
+					if (b)
+					{
+						SetForegroundWindow((HWND)p->MainWindowHandle.ToPointer());
+						break;
+					}
+				}
+			}
+			return b;
+		}
+	protected:
+		property Threading::Mutex ^Mutex;
+		property Diagnostics::Process ^MyProcess
+		{
+		protected: Diagnostics::Process ^get()
+		{
+			return FMyProcess;
+		}
+		private: void set(Diagnostics::Process ^value)
+		{
+			FMyProcess = value;
+		}
+		}
+	private:
+		Diagnostics::Process ^FMyProcess;
+	};
+}
---
+++ ApplicationActivator/ApplicationActivatorTester/App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+
+namespace ApplicationActivatorTester
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        public string ProgramName { get; set; }
+        private ApplicationActivator.ApplicationActivator Activator { get; set; }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var fn = Path.GetFileName(Environment.GetCommandLineArgs()[0]);
+            var an = Path.ChangeExtension(fn, null);
+            an = Path.ChangeExtension(an, null);
+            var avr = new ApplicationActivator.ApplicationActivator();
+            this.Activator = avr;
+            this.ProgramName = an;
+            if (avr != null)
+            {
+                if (!avr.Activate())
+                {
+                    MainWindow mw = new MainWindow();
+                    mw.Title = an;
+                    mw.Show();
+                }
+                else
+                {
+                    avr.Dispose();
+                    this.Shutdown();
+                }
+            }
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            var avr = this.Activator;
+            if (avr != null)
+            {
+                avr.Dispose();
+            }
+        }
+    }
+}
